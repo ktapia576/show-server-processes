6944872e310e3a2fd40b7975c1f1141eade2dfd7
ktapia576 <ktapia576@hotmail.com>
2019-10-08 19:57:53 -0400

    Check whether to ignore case or not

--- show-processes.php
+++ show-processes.php
@@ -1,9 +1,18 @@
 <?php 
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $key=$_POST["key"];
-        
-        $command = "ps | grep '$key' ";
-        $output = shell_exec($command);
+        $case=$_POST["case"];
+
+        // Check if command is case sensitive
+        if($case === "case-sensitive"){      
+            $command = "ps | grep '$key' ";
+            $output = shell_exec($command);
+        } elseif($case === "ignore-case"){      
+            $command = "ps | grep -i '$key' ";
+            $output = shell_exec($command);
+        } else {
+            $output="Error: Problem with choosing case sensitivity";
+        }
 
         echo nl2br($output);
     } else {
