08f91a4a14b906ec43af625b96e5a5d070545f00
ktapia576 <ktapia576@hotmail.com>
2019-10-08 19:38:52 -0400

    Add security and grep command

--- show-processes.php
+++ show-processes.php
@@ -1,4 +1,12 @@
 <?php 
-    $key=$_GET["key"];
-    echo $key;
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $key=$_POST["key"];
+        
+        $command = "ps | grep '$key' ";
+        $output = shell_exec($command);
+
+        echo nl2br($output);
+    } else {
+        echo "You cant access this webpage seperatly";
+    }
 ?>
