7f24d58be9d68648781415af14852807aedd1276
ktapia576 <ktapia576@hotmail.com>
2019-10-08 20:49:50 -0400

    Add invert option and change code format

--- show-processes.php
+++ show-processes.php
@@ -1,21 +1,39 @@
 <?php 
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $key=$_POST["key"];
-        $case=$_POST["case"];
+        $case_sensitive=$_POST["case"]; // case sensitive will be true or false
+        $invert=$_POST["invert"];   // invert will be true or false
+
+        // change string to boolean (helps when string = "false" since php sets as true with (boolean) when string not empty)
+        $case_sensitive=filter_var($case_sensitive, FILTER_VALIDATE_BOOLEAN); 
+        $invert=filter_var($invert, FILTER_VALIDATE_BOOLEAN);
+
+        $command = "ps | grep ";    // Will always use this command
+        $options ="";    // Options will be selected by User
 
         // Check if command is case sensitive
-        if($case === "case-sensitive"){      
-            $command = "ps | grep '$key' ";
-            $output = shell_exec($command);
-        } elseif($case === "ignore-case"){      
-            $command = "ps | grep -i '$key' ";
-            $output = shell_exec($command);
+        if($case_sensitive){      
+            $options="";
+        } elseif(!$case_sensitive){      
+            $options="-i ";
         } else {
             $output="Error: Problem with choosing case sensitivity";
         }
 
-        echo nl2br($output);
+        // Check if command is invert matching or not
+        if($invert){      
+            $options=$options."-v ";    //concatenate options
+        } elseif(!$invert){      
+            $options=$options."";
+        } else {
+            $output="Error: Problem with choosing case sensitivity";
+        }
+
+        $command = $command.$options.$key;  // Concatenate command
+        $output = shell_exec($command); // Execute command in shell
+        //echo nl2br($output);
+        echo json_encode(array("result" => nl2br($output), "command" => $command)); 
     } else {
-        echo "You cant access this webpage seperatly";
+        echo "You cant access this webpage separately";
     }
 ?>
